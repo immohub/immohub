80c335d1ec5d13f0b7cf8e7e295a2bc244777255
Alexis DARNAT <alexis.darnat@gmail.com>
2019-06-11 08:17:32 +0200

    Fixs

--- server/lib/server/accounts/emails/confirmation_email.ex
+++ server/lib/server/accounts/emails/confirmation_email.ex
@@ -1,10 +1,13 @@
 defmodule Server.Accounts.Emails.ConfirmationEmail do
+  # Imports
   import Bamboo.Email
+  # Aliases
+  alias Server.Accounts.User
 
   def perform(%User{} = user) do
     new_email()
     |> to(user.email)
     |> subject("Email Confirmation")
-    |> 
+    |> text_body("test")
   end
 end
